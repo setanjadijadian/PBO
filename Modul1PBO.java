b8e1ecc14cc9643b2fe033cbe83895de6e1a09e3
setanjadijadian <101535029+setanjadijadian@users.noreply.github.com>
2022-03-17 11:24:49 +0700

    Add files via upload

---
+++ Modul1PBO.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul1pbo;
+
+import java.util.*;
+
+/**
+ *
+ * @author ASUSVivoBook
+ */
+public class Modul1PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Soal Latihan 1
+/*       
+        System.out.println("hello");
+        System.out.println("selamat belajar program java");
+
+        //Soal Latihan 2
+        int Nilai_PBO,Nilai_WEB;
+        Nilai_PBO=90;
+        Nilai_WEB=80;
+        System.out.println("Variable yang terdapat dalam program : ");
+        System.out.println("Nilai_PBO = " + Nilai_PBO);
+        System.out.println("NIlai_WEB = " + Nilai_WEB);
+
+        //Soal Latihan 3 (ARITMATIKA)
+        int bil_a, bil_b, hasil;
+        bil_a = 454;
+        bil_b = 546;
+        hasil = bil_a + bil_b;
+        System.out.println("Hasil dari penjumlahan kedua bilangan : ");
+        System.out.println(hasil);
+
+        //Soal Latihan 3 (PERSEGI PANJANG)
+       
+        int p = 5;
+        int l = 10;
+        int t = 5;
+        System.out.println("Nilai Panjang Balok : " + p);
+        System.out.println("Nilai Luas Balok : " + l);
+        System.out.println("Nilai Tinggi Balok : " + t);
+        System.out.println("=======================================");
+        System.out.println("Menghitung Luas Permukaan Balok");
+        System.out.println("=======================================");
+        System.out.println("Luas Permukaan Balok : " + ((2*p*l)+(2*p*t)+(2*l*t)));
+
+       //Soal Latihan 3 (EKSPLORASI)
+        double bil_a, bil_b, hasil;
+        bil_a = 454.55;
+        bil_b = 546.55;
+        hasil = bil_a + bil_b;
+        System.out.println("Hasil dari penjumlahan kedua bilangan : ");
+        System.out.println(hasil);
+
+       //Soal Latihan 4
+       Scanner input = new Scanner(System.in);
+       int a,b;
+        System.out.print("Masukkan Nilai A : ");
+        a=input.nextInt();
+        System.out.print("Masukkan Nilai B : 10");
+        b=input.nextInt();
+        System.out.println("");
+        System.out.println("Nilai Variable Yang Ada Pada Program");
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+
+       //Soal Latihan 5
+       Scanner input = new Scanner(System.in);
+       int Nilai;
+        System.out.print("Masukkan Nilai Akhir PBO : ");
+        Nilai=input.nextInt();
+        if (Nilai < 70 )
+        System.out.println("Siswa TIDAK LULUS");
+         
+        if (Nilai > 70 ) 
+        System.out.println("Siswa LULUS");
+
+      //Soal Latian 6
+      Scanner input = new Scanner(System.in);
+       int Nilai;
+        System.out.print("Masukkan Nilai WEB : ");
+        Nilai=input.nextInt();
+        if (Nilai >= 90 )
+            System.out.println("NIlai A");
+        else if (Nilai >= 75 ) 
+            System.out.println("NIlai B");
+        else if (Nilai >= 50)
+            System.out.println("Nilai C");
+        else if (Nilai >= 35)
+            System.out.println("Nilai D");
+        else
+            System.out.println("Nilai E");
+
+       //Soal Latihan 7
+        Scanner input = new Scanner(System.in);
+       int bil;
+        System.out.print("Pilih Angka 1-3 : ");
+        bil=input.nextInt();
+        switch (bil) {
+            case 1 :
+            System.out.println("Satu");
+            break;
+            case 2 : 
+            System.out.println("Dua");
+            break;
+            case 3 : 
+            System.out.println("Tiga");
+            break;
+        }
+ 
+      //Soal Latihan 8
+       Scanner input = new Scanner(System.in);
+       int TPA, BAHASAINGGRIS;
+        System.out.print("Masukkan Nilai TPA : ");
+        TPA=input.nextInt();
+        System.out.print("Masukkan Nilai Bahasa Inggris : ");
+        BAHASAINGGRIS=input.nextInt();
+        if ((TPA > 85)&&(BAHASAINGGRIS>80))
+        System.out.println("Siswa Dapat Beasiswa");
+        else 
+        System.out.println("Siswa Tidak Dapat Beasiswa");
+
+     //Soal Latihan 9 
+        System.out.println("Semangat Pagi!");
+        for(int a=1; a<6; a++)
+            System.out.println("PAGI!!!");
+
+    //Soal Latihan 10
+    Scanner input = new Scanner(System.in);
+      
+        System.out.print("Masukkan Batas Bilangan : ");
+        int Batas = input.nextInt();
+        int Hasil=0;
+        for(int i=1; i<=Batas;i++)
+         Hasil += i;
+        System.out.println("Total Jumlahnya Adalah : " + Hasil);
+
+    //Soal Latihan 11
+    Scanner masukan = new Scanner(System.in);
+        int a=0,total=0,bil=7;
+        while (bil!=0){
+        a++;
+            System.out.print("Masukan bilangan ke-" +a+" : ");
+            bil = masukan.nextInt();
+            total += bil;
+    }
+        System.out.print("Total jumlah "+(a-1)+" bilangan :  ");
+        System.out.println(total);
+   //QUIZ 1
+        Scanner input = new Scanner(System.in);
+       
+       double r, LPL;
+       
+       
+        System.out.print("Masukkan Jari-Jari lingkaran : ");
+        r=input.nextInt();
+      double lpl= 4*Math.PI*Math.pow(r, 2);
+        System.out.println("Luas Permukaan Bola Adalah : " + lpl );
+      //QUIZ 2
+        int c = 78;
+        System.out.println("Convert Celcius to Reamur : " +CtoR(c));
+        System.out.println("Convert Celcius to Fahrenheit : " +CtoF(c));
+        System.out.println("Convert Celcius to Kelven : " +CtoK(c));
+    }
+    static double CtoR (double c){
+        return 0.8*c;
+    }
+    static double CtoF (double c) {
+        return 1.8*c+32;
+    }
+    static double CtoK (double c) {
+        return c+273;
+    */
+
+    }
+}
+    
+    
+
